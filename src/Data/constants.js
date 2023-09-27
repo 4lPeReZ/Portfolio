c793f0174ab4a461f26f372972ea313fea0c416d
4lPeReZ <alvaropr05@hotmail.com>
2023-09-27 21:03:50 +0200

    Bio data section and styles

---
+++ src/Data/constants.js
@@ -0,0 +1,19 @@
+// Bio data section that appears in the home section.
+export const Bio = {
+    name: "4lPeReZ",
+    roles: [
+        "Front-End Developer",
+        "Back-End Developer",
+        "Full Stack Developer",
+        "Software Developer",
+    ],
+    description:
+        "I'm passionate about computer science and programming. While I still consider myself a novice in this world, my curiosity is insatiable, and I'm always seeking to stay updated with the latest advancements in technology and development. I strive to be a proactive and creative individual, and I have confidence in my ability to tackle and solve challenges.",
+    github: "https://github.com/4lPeReZ",
+    resume:
+        "https://drive.google.com/file/d/1Zcung2OviIyzcHkWsM-crYhiWCXLwkSR/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/álvaro-pérez-rey-6337a6150/",
+    twitter: "https://twitter.com/4lPeReZ",
+    insta: "https://www.instagram.com/4lPeReZ/",
+    facebook: "https://www.facebook.com/",
+};
