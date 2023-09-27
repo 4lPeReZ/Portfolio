01656e241510d8306477e838b314d51d7960b289
4lPeReZ <alvaropr05@hotmail.com>
2023-09-27 21:08:51 +0200

    Education component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import { darkTheme } from './Utils/Themes';
 import Navbar from './Components/Navbar';
 import Hero from './Components/HeroSection';
 import Skills from './Components/Skills';
+import Education from './Components/Education';
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
 `
 
 function App() {
@@ -17,7 +24,10 @@ function App() {
       <Navbar />
       <Body>
         <Hero />
-        <Skills />
+        <Wrapper>
+          <Skills />
+          <Education />
+        </Wrapper>
       </Body>
     </ThemeProvider>
   );
