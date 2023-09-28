148366954b5d2f9bbecd1a8052ff1943f6d67ffc
4lPeReZ <alvaropr05@hotmail.com>
2023-09-28 12:36:49 +0200

    Skills section

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Container, Wrapper, Title, Desc, SkillsContainer, Skill, SkillTitle, SkillList, SkillItem, SkillImage } from "./SkillsStyledComponent";
+import { skills } from "../../Data/constants";
 
 const Skills = () => {
-    return <div>Skills</div>;
-};
-
+    return (
+      <Container id="skills">
+        <Wrapper>
+          <Title>Skills</Title>
+          <Desc>Here are some of my skills on which I have been working on for the past year.
+          </Desc>
+          <SkillsContainer>
+            {skills.map((skill) => (
+              <Skill>
+                <SkillTitle>{skill.title}</SkillTitle>
+                <SkillList>
+                  {skill.skills.map((item) => (
+                    <SkillItem>
+                      <SkillImage src={item.image}/>
+                      {item.name}
+                    </SkillItem>
+                  ))}
+                </SkillList>
+              </Skill>
+            ))}
+  
+          </SkillsContainer>
+        </Wrapper>
+      </Container>
+    )
+  }
 export default Skills;
