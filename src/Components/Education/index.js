ac76fed83f5b0af34a4eefadcef1302ad03fa479
4lPeReZ <alvaropr05@hotmail.com>
2023-09-29 11:51:35 +0200

    Education section and fix bugs

--- src/Components/Education/index.js
+++ src/Components/Education/index.js
@@ -1,7 +1,42 @@
-import React from "react";
 
-const Education = () => {
-    return <div>Education</div>;
-};
+import React from 'react'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { Container, Wrapper, Title, Desc, TimelineSection } from './EducationStyledComponent';
+import { education, experiences } from '../../Data/constants';
+import EducationCard from '../Cards/EducationCard';
 
-export default Education;
+const index = () => {
+    return (
+        <Container id="education">
+            <Wrapper>
+                <Title>Education</Title>
+                <Desc>
+                    My education has been a journey of self-discovery and growth. My educational details are as follows.
+                </Desc>
+                <TimelineSection>
+                    <Timeline>
+                        {education.map((education,index) => (
+                            <TimelineItem >
+                                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                    <EducationCard education={education}/>
+                                </TimelineContent>
+                                <TimelineSeparator>
+                                    <TimelineDot variant="outlined" color="secondary" />
+                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                </TimelineSeparator>
+                            </TimelineItem>
+                        ))}
+                    </Timeline>
+
+                </TimelineSection>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default index
